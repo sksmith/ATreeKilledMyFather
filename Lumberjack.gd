f70035b9246bebaf62f5c8594fd22089cb7baf84
Jim South <james.a.south@gmail.com>
2023-02-04 18:03:31 -0500

    Planting trees

--- Lumberjack.gd
+++ Lumberjack.gd
@@ -34,7 +34,7 @@ func _process(delta):
 	$PivotPoint.rotation = velocity.angle()  #local with local axis
 
 
-func _on_Player_body_entered(body):
+func _on_Player_body_entered(_body):
 	hide() # Player disappears after being hit.
 	emit_signal("hit")
 	# Must be deferred as we can't change physics properties on a physics callback.

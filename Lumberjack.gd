1732e3c1e5074d680c4f21b05474444aae82eb97
Sean K Smith <ssmith2347@gmail.com>
2023-02-04 13:48:14 -0500

    adding tree images

--- Lumberjack.gd
+++ Lumberjack.gd
@@ -3,7 +3,10 @@ extends Area2D
 signal hit
 
 export var speed = 400 # How fast the player will move (pixels/sec).
+export var attack_duration_ms = 0.5
 var screen_size # Size of the game window.
+var is_attacking = false
+var current_attack_time = 0
 
 func start(pos):
 	position = pos
@@ -16,23 +19,34 @@ func _ready():
 	hide()
 
 func _process(delta):
-	var velocity = Input.get_vector("lj_aim_left", "lj_aim_right", "lj_aim_up", "lj_aim_down")
-
-	if velocity.length() > 0:
-		velocity = velocity * speed
+	if !is_attacking && Input.is_action_pressed("attack"):
+		is_attacking = true
+		current_attack_time = 0
+		$AnimatedSprite.animation = "attack"
 	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.flip_h = velocity.x > 0
-	else:
-		$AnimatedSprite.animation = "idle"
+	if is_attacking:
+		current_attack_time += delta
 		
-	$PivotPoint.rotation = velocity.angle()  #local with local axis
+		if current_attack_time >= attack_duration_ms:
+			is_attacking = false
+			$AnimatedSprite.animation = "idle"
+	else:
+		var velocity = Input.get_vector("lj_aim_left", "lj_aim_right", "lj_aim_up", "lj_aim_down")
 
+		if velocity.length() > 0:
+			velocity = velocity * speed
+		
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
+
+		if velocity.x != 0:
+			$AnimatedSprite.animation = "walk"
+			$AnimatedSprite.flip_h = velocity.x > 0
+		else:
+			$AnimatedSprite.animation = "idle"
+			
+		$PivotPoint.rotation = velocity.angle()  #local with local axis
 
 func _on_Player_body_entered(body):
 	hide() # Player disappears after being hit.
